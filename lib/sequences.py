6125b7fecc792324310d56639fc659acaad770d4
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 20:02:35 +0300

    Completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        fib_sequence = []
+    elif length == 1:
+        fib_sequence = [0]
+    else:
+        fib_sequence = [0, 1]
+
+        while len(fib_sequence) < length:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+    
+    print(fib_sequence)
+
+# Test the function
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
+print_fibonacci(10)
